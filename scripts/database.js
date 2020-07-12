b1dd196836d5e71973b645066318342361bfaa3d
Mariana Mazzariello <mfmazzariello@gmail.com>
2020-07-13 00:36:55 +0200

    saveNewUser Database localStorage

--- scripts/database.js
+++ scripts/database.js
@@ -1,1 +1,34 @@
 "use strict";
+
+class Database {
+  //recuperar los usuarios - el array
+  getAllUsers = () => {
+    //recuperar el string
+    const usersStr = localStorage.getItem("users");
+    //convertir el string a un array
+    const usersArr = JSON.parse(userStr);
+
+    //si todavia no hay usuarios, devuelve el array vacio
+    if (usersArr === null) {
+      return [];
+    } else {
+      return usersArr;
+    }
+  };
+
+  saveNewUser = (newUser) => {
+    //recuperar el array de los usuarios del localstorage
+    const usersArr = this.getAllUsers();
+
+    //Actualizar el array de usuarios
+    usersArr.push(newUser);
+
+    //convertir el array a un string
+    const usersStr = JSON.stringify(usersArr);
+
+    //almacenarlo de nuevo
+    localStorage.setItem("users", usersStr);
+  };
+}
+
+const db = new Database();
