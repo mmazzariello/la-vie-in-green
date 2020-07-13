45cb78f6ae98ead93a983bebc8288f222a0d05aa
Mariana Mazzariello <mfmazzariello@gmail.com>
2020-07-13 12:54:09 +0200

    finish all validate's methods

--- scripts/validator.js
+++ scripts/validator.js
@@ -17,38 +17,81 @@ class Validator {
     };
   }
 
-
-
   //validar el nombre del email
   validateValidEmail = (email) => {
-  
-  const emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
     //comprueba si el email es valido, y si es valido, tengo que borrar el error del objeto this.erros
-  
-if () {
+    if (this.emailIsValid(email)) {
+      delete this.errors.invalidEmailError;
+    } else {
+      // si no es valido, poner el mensaje
+      this.errors.invalidEmailError = this.invalidEmailError;
+    }
+  };
 
-}
+  //funcion auxiliar de validateEmail
+  emailIsValid = (email) => {
+    //RegEx objeto especial que contiene las reglas de la sintaxis que el mail tiene que cumplir
+    const emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
 
-  // si no es valido, poner el mensaje 
-  
-  }
+    //metodo test prueba si la cadena cumple las reglas
+    //devuelve true o false
+    const isValid = emailRegEx.test(email);
 
-  //validar que el email no esta tomado, es decir, es unico
-  emailIsValid = (email) => {}
+    return isValid;
+  };
+
+  //validar que el email no esta tomado,
+  //validar si el email es unico
+  validateUniqueEmail = (newEmail) => {
+    const usersDB = db.getAllUsers();
+
+    let emailUnique = true;
+
+    if (usersDB.length > 0) {
+      usersDB.forEach((userObj) => {
+        //si el email ya esta tomado, cambia el valor de la variable a false
+        if (userObj.email === newEmail) {
+          emailUnique = false;
+        }
+      });
+
+      if (emailUnique) {
+        //quitar el mensaje de error
+        delete this.errors.emailExistsError;
+      } else {
+        //si el mail no es unico, poner el mensaje de nuevo
+        this.errors.emailExistsError = this.emailExistsError;
+      }
+    }
+  };
 
   //metodo para validar la lomgitud del password
-  validatePassword = (password) => {}
+  validatePassword = (password) => {
+    if (password.length > 5) {
+      //quita el mensaje
+      delete this.errors.passwordError;
+    } else {
+      // si el password tiene menos de 5 caracteres, poner el mensaje
+      this.errors.passwordError = this.passwordError;
+    }
+  };
 
   //validar si el password coincide con repeat password
-  validatePasswordRepeat = (password) => {};
+  validatePasswordRepeat = (password, passwordRepeat) => {
+    if (password === passwordRepeat) {
+      delete this.errors.repeatPasswordError;
+    } else {
+      this.errors.repeatPasswordError = this.repeatPasswordError;
+    }
+  };
 
   //este metodo sirve para obtener el objeto con errores, y mostrarle al usr en signuo
   getErrors = () => {
     return this.errors;
-  }
+  };
 
   //reiniciar los errores mostrados
-  resetValidator = () => {}
+  resetValidator = () => {};
 }
 
 const validator = new Validator();
