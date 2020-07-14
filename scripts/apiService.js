9efc9c40f70968f288f8f7f9957d438ed00b9e02
Mariana Mazzariello <mfmazzariello@gmail.com>
2020-07-14 15:24:51 +0200

    create elements in apiService js

--- scripts/apiService.js
+++ scripts/apiService.js
@@ -1,32 +1,45 @@
 "use strict";
 
-// function getIngredients () {
-//   //
+function getIngredients() {
+  const section = document.querySelector(".ingredients-list");
+  const article = document.createElement("article");
 
-// const section = document.querySelector(".ingredient-list");
+  const getRecipes = () => {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=tomato,broccoli,onion,carrot,zucchini,potato,spinach,lettuce`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        
+        if ("${data.thumbnail}".length > 0) {
+          article.innerHTML = data
+            .map(
+              (data) => ` <img src="${data.thumbnail}" alt="${data.name}" />
+    <p>${data.ingredients}</p>
+    <h3>${data.title}</h3>`
+            )
+            .join("");
+        }
 
-const getRecipes = () => {
-  return fetch(`https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=tomato,broccoli,onion,carrot,zucchini,potato,spinach,lettuce`
-)
-    .then((res) => res.json())
-    .then((data) => {
-      return data.results;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-function awaitAll(count, asyncFn) {
-  const promises = [];
-  for (let i = 0; i < count; ++i) {
-    promises.push(asyncFn());
+        section.appendChild(article);
+
+        // return data.results;
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  function awaitAll(count, asyncFn) {
+    const promises = [];
+    for (let i = 0; i < count; ++i) {
+      promises.push(asyncFn());
+    }
+    return Promise.all(promises);
   }
-  return Promise.all(promises);
+  awaitAll(11, getRecipes)
+    .then((results) => console.log("asyncFinished", results))
+    .catch((e) => console.error(e));
 }
-awaitAll(11, getRecipes)
-  .then((results) => console.log("asyncFinished", results))
-  .catch((e) => console.error(e));
 
-
-
-// getIngredients();
+getIngredients();
