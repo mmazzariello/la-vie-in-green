f200e91fcde09632a80c77107804ee73f40dcb86
Mariana Mazzariello <mfmazzariello@gmail.com>
2020-07-14 20:23:18 +0200

    finally version apiservicejs it's ok

--- scripts/apiService.js
+++ scripts/apiService.js
@@ -1,16 +1,22 @@
 "use strict";
 
-// function getIngredients() {
+const arr = [
+  "http://www.recipepuppy.com/api/?i=tomato,broccoli",
+  "http://www.recipepuppy.com/api/?i=onion,carrot",
+  "http://www.recipepuppy.com/api/?i=zucchini,potato",
+  "http://www.recipepuppy.com/api/?i=spinach,lettuce",
+];
+
 const getRecipes = () => {
   return fetch(
-    `https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=tomato,broccoli,onion,carrot,zucchini,potato,spinach,lettuce`
+    `https://cors-anywhere.herokuapp.com/${
+      arr[Math.floor(Math.random() * arr.length)]
+    }`
   )
     .then((res) => res.json())
     .then((data) => {
-    console.log("hola", data);
       return data.results;
     })
-
     .catch((error) => {
       console.log(error);
     });
@@ -25,42 +31,21 @@ function awaitAll(count, asyncFn) {
 
 const section = document.querySelector(".ingredients-list");
 
-awaitAll(10, getRecipes)
+awaitAll(8, getRecipes)
   .then((results) => {
     for (let i = 0; i < results.length; i++) {
       results[i].forEach((recipe) => {
         if (recipe.thumbnail !== "") {
-          // console.log(recipe);
           const article = document.createElement("article");
-          article.innerHTML = `<h3>${recipe.title}</h3>
+
+          article.innerHTML = `
+          <h3>${recipe.title}</h3>
         <img src="${recipe.thumbnail}" alt="${recipe.title}" />
           <p>${recipe.ingredients}</p>`;
+
           section.appendChild(article);
         }
       });
-      // for (let x = 0; x < results.length; x ++) {
-      //   if (results[i][x].thumbnail !== "") {
-      //  let recipe= results[i][x];
-      //     article.innerHTML = `<h3>${recipe.title}</h3>
-      //     <img src="${recipe.thumbnail}" alt="${recipe.title}" />
-      //       <p>${recipe.ingredients}</p>`
-      //   }
-      // }
     }
-    // .join("");
-    // results
-    //   .map((recipearray) => {
-    // recipearray.map((recipe) => {
-    //     if (recipe.thumbnail !== "") {
-
-    //     }
-    // })
-
-    // })
-
-    // console.log("asyncFinished", results);
   })
   .catch((e) => console.error(e));
-// }
-
-// getIngredients();
